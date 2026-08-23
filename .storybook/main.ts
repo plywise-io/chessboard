f6e0df4e6407d440bca70c1bb4f5dbfc4d7ae32c
Corentin GS <contact@corentings.dev>
2026-08-23 16:30:41 +0200

    chore: add Storybook docs addon for autodocs rendering

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,6 +3,7 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   framework: "@storybook/react-vite",
   stories: ["../packages/chessboard-react/stories/**/*.stories.tsx"],
+  addons: ["@storybook/addon-docs"]
 };
 
 export default config;
